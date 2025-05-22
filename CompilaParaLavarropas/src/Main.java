d06b1b89ea890f13591ed86c75215f556267e441
Ezequiel-Rickert <ezequiel.rickert@alumnos.frgp.utn.edu.ar>
2025-05-22 20:21:39 -0300

    Inicio pruebas merge
    -Carga de Proyecto y declaracion de variables en el main.

---
+++ CompilaParaLavarropas/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numero = 1;
+		
+		String palabra = "Sopa";
+		
+		List<String> listaEspecialidades = new ArrayList<String>();
+		
+		//Comentario de ejemplo - push a main
+		
+	}
+
+}
